f25cf3a6244575df924faf58d031ba9494fd2075
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2023-05-15 23:42:21 -0500

    Commit del Modelo

--- projectoIngrador/muralista/models.py
+++ projectoIngrador/muralista/models.py
@@ -1,45 +1,40 @@
 from django.db import models
 
 # Create your models here.
-       
-class Contacto(models.Model):
-    celular = models.CharField(max_length=9) 
-    instagram = models.CharField(max_length=100)
-    facebook= models.CharField(max_length=200)     
-    def _str_(self):
-         return self.celular
-   
-class Estilo(models.Model):
-    nombre_color = models.CharField(max_length=200)  
-    
-     
+           
 class Muralista(models.Model):
-    nombre = models.CharField(max_length=200) 
-    apellidos = models.CharField(max_length=200)
-    seudonimo = models.CharField(max_length=200)
+    id_muralista = models.CharField(primary_key=True, max_length=100)
+    nombre = models.CharField(max_length=50) 
+    apellidos = models.CharField(max_length=50)
+    seudonimo = models.CharField(max_length=50)
     foto = models.BinaryField(max_length=10, default=b'\x08')
-    contacto = models.ForeignKey(Contacto , on_delete=models.CASCADE) #oneToOne (pk)
-    estilo = models.ForeignKey(Estilo, on_delete=models.CASCADE)
+    murales  = models.CharField(max_length=200)
+    celular = models.IntegerField()
+    instagram = models.CharField(max_length=50)
+    facebook = models.CharField(max_length=50)
+    correo = models.EmailField()
     
-    def _str_(self):
-         return self.nombre
     
-class Color(models.Model):
+class Mural(models.Model):
+     idMural = models.CharField(primary_key=True, max_length=100)
+     nombre_mural = models.CharField(max_length=50)
+     direccion = models.CharField(max_length=100)
+     date = models.DateField()
+     imagen = models.BinaryField(max_length=10, default=b'\x08')
+     descripcion = models.CharField(max_length=100)
+     id_muralista = models.ForeignKey(Muralista, on_delete=models.CASCADE) 
+
     
-    idColor = models.CharField(primary_key=True, max_length=6)
-    nombre = models.CharField(max_length=200) 
-    rgb = models.CharField(max_length=200) 
+class Colores(models.Model):
+    idColores = models.CharField(primary_key=True, max_length=6)
+    nombre_color = models.CharField(max_length=200) 
+    codigo = models.CharField(max_length=20)
     red= models.CharField(max_length=200) 
     green = models.CharField(max_length=200) 
     blue = models.CharField(max_length=200) 
     
-    def _str_(self):
-            return self.nombre
-  
 class Paleta(models.Model):
-    idPaleta = models.CharField(primary_key=True, max_length=6)
-    color = models.ForeignKey(Color, on_delete=models.CASCADE) 
-    muralista = models.ForeignKey(Muralista , on_delete=models.CASCADE)  
+    idMural = models.ForeignKey(Mural, on_delete=models.CASCADE) 
+    idColores = models.ForeignKey(Colores, on_delete=models.CASCADE) 
 
-class Ejemplo(models.Model):
-     ejemplo = models.CharField(max_length=200)    
+
